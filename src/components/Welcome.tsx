d7f0370ce3fb74d94f870902063639d3fee64db1
Stephen Kettley <kettleystephen@gmail.com>
2024-01-13 12:11:36 +0200

    insert emoji into welcome bar

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,7 +4,9 @@ const Welcome = () => {
   return (
     <div className={styles["welcome-container"]}>
       <div className={styles.container}>
-        <p className={styles["welcome-back"]}>Welcome Back, John!</p>
+        <p className={styles["welcome-back"]}>
+          Welcome Back, John! <span>&#x1F44B;</span>
+        </p>
         <h1 className={styles["company-name"]}>Company Name</h1>
       </div>
     </div>
