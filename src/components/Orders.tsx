259f99fac957926c37751402d40006bcf4bfe551
Stephen Kettley <kettleystephen@gmail.com>
2024-01-14 17:57:10 +0200

    setup structure for orders

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,7 +1,15 @@
 import styles from "./Orders.module.scss";
 
 const Orders = () => {
-  return <div className={styles["orders-container"]}></div>;
+  return (
+    <div className={styles["orders-container"]}>
+      <div className={styles.header}>
+        <p>Today's Orders</p>
+        <p>Place Order</p>
+      </div>
+      <p>1 total</p>
+    </div>
+  );
 };
 
 export default Orders;
