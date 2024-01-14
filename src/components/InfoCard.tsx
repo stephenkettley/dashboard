05a7607da229ed4cf7e293ec94b2f5df7c095f95
Stephen Kettley <kettleystephen@gmail.com>
2024-01-14 17:56:58 +0200

    setup placement for info cards

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,8 +1,24 @@
 import styles from "./InfoCard.module.scss";
 
-const InfoCard = ({ cardType }) => {
-  const card = cardType;
-  return <div className={`${styles[card]}`}></div>;
-};
-
-export default InfoCard;
+export default function InfoCard(props: {
+  cardType: string;
+  amount: number;
+  icon: string;
+  action: string;
+}) {
+  const card = props.cardType;
+  return (
+    <div className={`${styles[card]}`}>
+      <div className={styles["info-card-top"]}>
+        <p>{props.icon}</p>
+        <p>{props.action}</p>
+      </div>
+      <div className={styles["info-card-bottom"]}>
+        <p>
+          {props.cardType.charAt(0).toUpperCase() + props.cardType.slice(1)}
+        </p>
+        <p className={styles.amount}>{props.amount}</p>
+      </div>
+    </div>
+  );
+}
